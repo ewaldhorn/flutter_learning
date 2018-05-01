fc70c4df5ff3f599a7e1a806289a57da4839e7db
Ewald Horn <ewaldhorn@gmail.com>
2018-05-01 21:32:51 +0200

    Functional Dart

--- codelabs/DartFunctional.dart
+++ codelabs/DartFunctional.dart
@@ -4,5 +4,17 @@ String scream(int length) {
 
 main() {
   var values = [1,2,3,5,10,50];
+  
+  // imperative (non-functional)
+  print("Imperative mode:");
+  for (var length in values) {
+    print(scream(length));
+  }
+
+  print("\nFunctional mode(basic):");
+  values.map(scream).forEach(print);
+
+  // skip first value, take next 3, others are skipped
+  print("\nFunctional mode(advanced):");
   values.skip(1).take(3).map(scream).forEach(print);
 }
