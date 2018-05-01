004f0748c6b78c05232b9d7dc6a4d3a9f88270a1
Ewald Horn <ewaldhorn@gmail.com>
2018-05-01 21:18:19 +0200

    Added some implementation.

---
+++ codelabs/Interfaces.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+abstract class Shape {
+  factory Shape(String type) {
+    if (type == 'circle') return new Circle(2);
+    if (type == 'square') return new Square(2);
+    throw 'Can\'t create $type.';
+  }
+  num get area;
+}
+
+class Circle implements Shape {
+  final num radius;
+  Circle(this.radius);
+  num get area => PI * pow(radius, 2);
+}
+
+class CircleMock implements Circle {
+  num area, radius;
+}
+
+class Square implements Shape {
+  final num side;
+  Square(this.side);
+  num get area => pow(side, 2);
+}
+
+main() {
+  var circle = new Circle(2);
+  var square = new Square(2);
+  print(circle.area);
+  print(square.area);
+}
