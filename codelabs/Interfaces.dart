0ecac0b7e0a27179aff28ee9650d06c3cabcbd30
Ewald Horn <ewaldhorn@gmail.com>
2018-05-02 13:19:16 +0200

    Fixing a deprecated PI reference

--- codelabs/Interfaces.dart
+++ codelabs/Interfaces.dart
@@ -12,7 +12,7 @@ abstract class Shape {
 class Circle implements Shape {
   final num radius;
   Circle(this.radius);
-  num get area => PI * pow(radius, 2);
+  num get area => pi * pow(radius, 2);
 }
 
 class CircleMock implements Circle {
