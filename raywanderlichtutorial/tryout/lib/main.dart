1247ea9c3be79b5680f9c8e56a3c2541676c966b
Ewald Horn <ewaldhorn@gmail.com>
2018-05-14 13:36:16 +0200

    Add network call. Parse data. Display data in list.

--- raywanderlichtutorial/tryout/lib/main.dart
+++ raywanderlichtutorial/tryout/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'strings.dart';
 
@@ -23,13 +25,42 @@ class MyWidget extends StatefulWidget {
 }
 
 class MyWidgetState extends State<MyWidget> {
+  var _members = [];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  _loadData() async {
+    String dataURL = "https://api.github.com/orgs/raywenderlich/members";
+    http.Response response = await http.get(dataURL);
+    setState(() {
+      _members = JSON.decode(response.body);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loadData();
+  }
+
+  Widget _buildRow(int i) {
+    return new ListTile(
+        title: new Text("${_members[i]["login"]}", style: _biggerFont));
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(Strings.appTitle),
       ),
-      body: new Text('This is my widget!'),
+      body: new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: _members.length,
+        itemBuilder: (BuildContext context, int position) {
+          return _buildRow(position);
+        },
+      ),
     );
   }
 }
