ec450bf49527907fa287a153a61138fe8c8b0178
Ewald Horn <ewaldhorn@gmail.com>
2018-05-14 12:00:01 +0200

    Removing default code and adding tutorial code.

--- raywanderlichtutorial/tryout/test/widget_test.dart
+++ raywanderlichtutorial/tryout/test/widget_test.dart
@@ -10,9 +10,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:tryout/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Flutter tutorial smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+    await tester.pumpWidget(new LearningFlutterApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
